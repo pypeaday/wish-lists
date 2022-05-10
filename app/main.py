300390c8a52ea63532068882df07c141cae741b7
Nicholas Payne <nic.payne@protonmail.com>
2022-05-10 15:43:10 -0500

    change api routs to lead with /api

--- app/main.py
+++ app/main.py
@@ -35,21 +35,18 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-
-
-
 from app.model.model import Wishes
 from app.schema.schema import patch_schema, wish_schema
 from app.session.session import create_get_session
 
 
-@app.get("/wishes", response_model=List[wish_schema], status_code=200)
+@app.get("/api/wishes", response_model=List[wish_schema], status_code=200)
 async def read_wishes(db: Session = Depends(create_get_session)):
     wishes = db.query(Wishes).all()
     return wishes
 
 
-@app.post("/wishes", response_model=wish_schema, status_code=201)
+@app.post("/api/wishes", response_model=wish_schema, status_code=201)
 async def add_wish(wish: wish_schema, db: Session = Depends(create_get_session)):
     new_wish = Wishes(
         person=wish.person,
@@ -65,13 +62,13 @@ async def add_wish(wish: wish_schema, db: Session = Depends(create_get_session))
     return new_wish
 
 
-@app.get("/wishes/{id}", response_model=wish_schema, status_code=200)
+@app.get("/api/wishes/{id}", response_model=wish_schema, status_code=200)
 async def get_wish(id: int, db: Session = Depends(create_get_session)):
     wish = db.query(Wishes).get(id)
     return wish
 
 
-@app.patch("/wishes/{id}", response_model=wish_schema, status_code=200)
+@app.patch("/api/wishes/{id}", response_model=wish_schema, status_code=200)
 async def update_wish(
     id: int, patch: patch_schema, db: Session = Depends(create_get_session)
 ):
@@ -84,7 +81,7 @@ async def update_wish(
     return db_wish
 
 
-@app.delete("/wishes/{id}", status_code=200)
+@app.delete("/api/wishes/{id}", status_code=200)
 async def delete_wish(id: int, db: Session = Depends(create_get_session)):
     db_wish = db.query(Wishes).get(id)
     if not db_wish:
