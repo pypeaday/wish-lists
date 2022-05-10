840167cfb39fb23c50e0127423b65ab9da4de2ef
Nicholas Payne <nic.payne@protonmail.com>
2022-05-10 08:42:44 -0500

    rename existing modules to be under app

--- app/main.py
+++ app/main.py
@@ -28,3 +28,73 @@ async def home(request: Request):
 async def page(request: Request, page_name: str):
     data = openfile(page_name + ".md")
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from model import Wishes
+from schema import patch_schema, wish_schema
+from session import create_get_session
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "server is up!"}
+
+
+@app.get("/wishes", response_model=List[wish_schema], status_code=200)
+async def read_wishes(db: Session = Depends(create_get_session)):
+    wishes = db.query(Wishes).all()
+    return wishes
+
+
+@app.post("/wishes", response_model=wish_schema, status_code=201)
+async def add_wish(wish: wish_schema, db: Session = Depends(create_get_session)):
+    new_wish = Wishes(
+        person=wish.person,
+        item=wish.item,
+        link=wish.link,
+        purchased=wish.purchased,
+        purchased_by=wish.purchased_by,
+        date_added=wish.date_added,
+    )
+    db.add(new_wish)
+    db.commit()
+
+    return new_wish
+
+
+@app.get("/wishes/{id}", response_model=wish_schema, status_code=200)
+async def get_wish(id: int, db: Session = Depends(create_get_session)):
+    wish = db.query(Wishes).get(id)
+    return wish
+
+
+@app.patch("/wishes/{id}", response_model=wish_schema, status_code=200)
+async def update_wish(
+    id: int, patch: patch_schema, db: Session = Depends(create_get_session)
+):
+    db_wish = db.query(Wishes).get(id)
+    db_wish.purchased = patch.purchased
+    db_wish.purchased_by = patch.purchased_by
+    db.commit()
+    db.refresh(db_wish)
+
+    return db_wish
+
+
+@app.delete("/wishes/{id}", status_code=200)
+async def delete_wish(id: int, db: Session = Depends(create_get_session)):
+    db_wish = db.query(Wishes).get(id)
+    if not db_wish:
+        raise HTTPException(status_code="404", detail="Wish id does not exist")
+
+    db.delete(db_wish)
+    db.commit()
+
+    return None
